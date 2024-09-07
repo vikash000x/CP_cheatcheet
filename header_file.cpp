d67c045cf255ab74de8b44eb2fb654aad0581094
vikash sinha <103680992+vikash000x@users.noreply.github.com>
2024-09-07 13:19:37 +0530

    Update header_file.cpp

--- header_file.cpp
+++ header_file.cpp
@@ -1,1 +1,14 @@
+#define ll long long int
+#define sorta(a) sort(a.begin(), a.end())
+#define cinn cin >> n
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define out(a) cout << a << endl
+#define fin(i, init, n) for (ll i = init; i < n; i++)
+#define ai(arr, n) for (int i = 0; i < n; i++) { cin >> arr[i]; }
+#define ao(arr, n) for (int i = 0; i < n; i++) { cout << arr[i] << " "; }
+#define vin(v, n) vector<int> v(n,0); for (int i = 0; i < n; i++) { cin >> v[i]; }
+const int MOD = 1e9 + 7;
 
+
+ 
